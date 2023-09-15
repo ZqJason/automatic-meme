2028442fe1cd33e94b03ff010245de73a6313c7e
ZqJason <145105502+ZqJason@users.noreply.github.com>
2023-09-15 17:01:51 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,13 +1,26 @@
+//copy this to cloudflare workers
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    url.host = "api.openai.com";
-    // openai is already set all CORS heasders 
-    return fetch(url, {
-      headers: request.headers,
-      method: request.method,
-      body: request.body,
-      redirect: 'follow'
-    });
-  }
-}
+    try {
+      const OPENAI_API_HOST = "api.openai.com";
+      const oldUrl = new URL(request.url);
+
+      if (oldUrl.pathname === "/") {
+        return new Response(`https://${oldUrl.hostname}/v1`, { status: 200 });
+      }
+
+      const newUrl = new URL(request.url);
+      newUrl.hostname = OPENAI_API_HOST;
+
+      const modifiedRequest = new Request(newUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body,
+      });
+
+      return await fetch(modifiedRequest);
+    } catch (e) {
+      return new Response(e.stack, { status: 500 });
+    }
+  },
+};
